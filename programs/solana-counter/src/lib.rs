07ece9d649425cf6208d84769f13027bd25fa672
Braulio Lomeli <brauliolomeli@gmail.com>
2024-05-16 14:16:40 -0500

    Counter program

--- programs/solana-counter/src/lib.rs
+++ programs/solana-counter/src/lib.rs
@@ -3,13 +3,44 @@ use anchor_lang::prelude::*;
 declare_id!("C1kF876QutJ1jAU8grXRy5H7ZiHnS5rW6nhYMRaWyfDJ");
 
 #[program]
-pub mod solana_counter {
+mod on_chain_counter {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        seeds = [b"counter".as_ref(), user.key().as_ref()],
+        bump,
+        payer = user,
+        space = 8 + 8
+    )]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
